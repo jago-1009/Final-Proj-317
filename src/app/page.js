dedc27584a3aed697949b004efa57eef3c1111a7
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2024-11-20 14:09:45 -0500

    Added Home Page and began Carousel. No animations.

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,61 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+ const icons = ["archer", "knight", "monk"];
+ const [icon, setIcon] = useState("archer");
+ const [prevIcon, setPrevIcon] = useState("monk");
+ const [nextIcon, setNextIcon] = useState("knight");
+ function changeIcon(direction) {
+  const currentIndex = icons.indexOf(icon);
+  let nextIndex
+  if (direction === "left") {
+    nextIndex = (currentIndex - 1);
+  }
+  if (direction === "right") {
+    nextIndex = (currentIndex + 1);
+  }
+  if (nextIndex > icons.length - 1) {
+    nextIndex = 0
+    setNextIcon(icons[nextIndex + 1]);
+    setPrevIcon(icons[icons.length - 1]);
+    setIcon(icons[nextIndex]);
+  }
+  else if (nextIndex == icons.length - 1) {
+    setNextIcon(icons[0]);
+    setIcon(icons[nextIndex]);
+    setPrevIcon(icons[nextIndex - 1]);
+  }
+  else if (nextIndex < 0) {
+    nextIndex = icons.length - 1
+    setPrevIcon(icons[nextIndex - 1]);
+    setNextIcon(icons[0]);
+    setIcon(icons[nextIndex]);
+    
+  }
+  else if (nextIndex === 0) {
+    setPrevIcon(icons[icons.length - 1]);
+    setNextIcon(icons[nextIndex + 1]);
+    setIcon(icons[nextIndex]);
+  }
+  else {
+    setIcon(icons[nextIndex]);
+    setPrevIcon(icons[nextIndex - 1]);
+    setNextIcon(icons[nextIndex + 1]);
+  }
+ }
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div className={styles.arrow} onClick={() => changeIcon("left")}>Change Me Left</div>
+      <div className={styles.icons}>
+      <h1 className={styles.iconL}>{prevIcon}</h1>
+        <h1 className={styles.iconC}>{icon}</h1>
+        <h1 className={styles.iconR}>{nextIcon}</h1>
+       
+      </div>
+      <div className={styles.arrow} onClick={() => changeIcon("right")}>Change Me Right</div>
     </div>
   );
 }
