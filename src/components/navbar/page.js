c791c54b21987730675e674f4d0a8a11d5317eb8
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2024-12-04 16:15:17 -0500

    Began Responsiveness of Nav Page.

--- src/components/navbar/page.js
+++ src/components/navbar/page.js
@@ -4,26 +4,48 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 import { SignedIn, SignInButton, SignedOut, UserButton } from '@clerk/nextjs';
+import { slide as Menu } from 'react-burger-menu';
+ function DesktopNav({}) {
+    const [isOpen, setOpen] = useState(false);
+    const width = window.innerWidth;
+  
+    if (width > 600) {
+        return <div className={styles.container}>
+        <div className={styles.navLinks}>
+            <Link href="/">Home</Link>
+            <Link href="/medieval">The Medieval Times</Link>
+            <Link href="/roles/archer">Archer</Link>
+            <Link href="/roles/knight">Knight</Link>
+            <Link href="/roles/monk">Priest</Link>
+        </div>
+
+        <div className={styles.signIn}>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+        </div>
+    </div>
+}
+else {
+    return <div className={styles.container}>
+        <Menu className={styles.menu}><div className={styles.navLinksMobile}>
+            <Link href="/">Home</Link>
+            <Link href="/medieval">The Medieval Times</Link>
+            <Link href="/roles/archer">Archer</Link>
+            <Link href="/roles/knight">Knight</Link>
+            <Link href="/roles/monk">Priest</Link>
+        </div></Menu>
+    </div>
+}
+}
 export default function Navbar() {
+    
+
     const [open, setOpen] = useState(false);
     return (
-        <div className={styles.container}>
-            <div className={styles.navLinks}>
-                <Link href="/">Home</Link>
-                <Link href="/medieval">The Medieval Times</Link>
-                <Link href="/roles/archer">Archer</Link>
-                <Link href="/roles/knight">Knight</Link>
-                <Link href="/roles/monk">Priest</Link>
-            </div>
-
-            <div className={styles.signIn}>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-                <SignedOut>
-                    <SignInButton />
-                </SignedOut>
-            </div>
-        </div>
+        <DesktopNav />
     );
 }
