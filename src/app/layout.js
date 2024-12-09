222544e0618ebbc493d161c57d58daf2f7f3fc02
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2024-12-09 10:16:47 -0500

    test for nav error.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,12 +3,16 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/navbar/page";
 import { DataProvider } from "@/hooks/getData";
+import dynamic from "next/dynamic";
+const NavbarComp = dynamic(() => import("@/components/navbar/page"), {
+  ssr:false
+})
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
     <html lang="en">
       <body >
-        <Navbar />
+        <NavbarComp />
         <DataProvider>
         {children}
         </DataProvider>
