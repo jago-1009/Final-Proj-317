357b4babfe059564816aa75f31b9c680623248c8
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2024-12-11 08:56:51 -0500

    Fixed Vercel bug with useSearchParams
    useSearchParams was throwing an error on the Vercel Build, so resolved the bug by using React Suspense.

--- src/app/contact/success/page.js
+++ src/app/contact/success/page.js
@@ -1,29 +1,37 @@
 "use client";
 import { useSearchParams } from "next/navigation";
 import styles from "../page.module.css";
+import { Suspense } from "react";
 
-export default function Success() {
-  const searchParams = useSearchParams();
+function Search() {
+  const searchParams = useSearchParams()
   const email = searchParams.get("email");
   const message = searchParams.get("message");
+  return <div className={styles.container}>
+  <h1 className={styles.pageHeading}>Message Sent Successfully!</h1>
+  <section className={styles.messageReviewContainer}>
+    <section className={styles.messageReview}>
+      <p className={styles.messageReviewText}>
+        <strong className={styles.messageParamTag}>Email:</strong> {email}
+      </p>
+      <p className={styles.messageReviewText}>
+        <strong className={styles.messageParamTag}>Message:</strong>{" "}
+        {message}
+      </p>
+    </section>
+  </section>
+  <a href="/contact" className={styles.contactReturn}>
+    Send another message
+  </a>
+</div>
 
+}
+export default function Success() {
+
+  
   return (
-    <div className={styles.container}>
-      <h1 className={styles.pageHeading}>Message Sent Successfully!</h1>
-      <section className={styles.messageReviewContainer}>
-        <section className={styles.messageReview}>
-          <p className={styles.messageReviewText}>
-            <strong className={styles.messageParamTag}>Email:</strong> {email}
-          </p>
-          <p className={styles.messageReviewText}>
-            <strong className={styles.messageParamTag}>Message:</strong>{" "}
-            {message}
-          </p>
-        </section>
-      </section>
-      <a href="/contact" className={styles.contactReturn}>
-        Send another message
-      </a>
-    </div>
+    <Suspense>
+      <Search />
+    </Suspense>
   );
 }
